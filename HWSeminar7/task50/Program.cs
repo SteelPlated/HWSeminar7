71ad1a26944aeacabe3046f06e783f433530250f
SteelPlated <mix.singatulin@yandex.ru>
2022-06-04 21:09:01 +0500

    1

---
+++ HWSeminar7/task50/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 50: Напишите программу, которая на вход
+принимает позиции элемента в двумерном массиве, и
+возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+Console.Write("Введите строку:");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Console.Write("Введите столбец:");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 4; int m = 3;
+Random random = new Random();
+int[,] array = new int[n, m];
+Console.WriteLine();
+void CreateArray(int[,] array)
+{
+        for (int i = 0; i < n; i++)
+        {
+        for (int j = 0; j < m; j++)
+            {
+            array[i, j] = random.Next(10, 99);
+            Console.Write("{0} ", array[i, j]);
+            }
+        Console.WriteLine();
+        }
+}
+void Search(int[,] array)
+{
+        if (pos1 < 0 | pos1 > n - 1 | pos2 < 0 | pos2 > m - 1)
+        {
+            Console.WriteLine("Такого элемента нет");
+        }
+        else
+        {
+            Console.WriteLine("Значение элемента массива = {0}", array[pos1, pos2]);
+        }
+}
+CreateArray(array);
+Search(array);
---
+++ HWSeminar7/task52/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 52: Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом
+столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого
+столбца: 4,6; 5,6; 3,6; 3.*/
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"m = {m}, n = {n}.");
+int[,] array = new int[m, n];
+Random random = new Random();
+
+
+
+void Calculations(int[,] array)
+{
+Console.Write($"\nCреднее арифметическое:\n");
+for (int i = 0; i < n; i++)
+{
+  double Mean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    Mean += array[j, i];
+  }
+  Mean = Math.Round(Mean / m, 1);
+  Console.WriteLine($"столбца № {i+1} {Mean}");
+}
+}
+
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.Next(1, 99);
+    }
+  }
+}
+
+void PrintArray (int[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(array[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
+}
+CreateArray(array);
+PrintArray(array);
+Calculations(array);
